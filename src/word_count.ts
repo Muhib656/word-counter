dd9d32d5f166a3fedd1bd2a2f1376e62f62cb5e9
Muhib Ahmed <a.muhib656@gmail.com>
2024-05-20 07:46:44 +0500

    first commit

---
+++ src/word_count.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+async function main() {
+        const userInput = await inquirer.prompt([
+            {
+                name: "paragraph",
+                type: "input",
+                message: chalk.yellow("Write your paragraph to count:")
+            }
+        ]);
+
+        const words = userInput.paragraph.trim().split(" ");
+        let wood = words.join("")
+        console.log(wood.length)
+}
+
+main();
